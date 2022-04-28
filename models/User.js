7f31a07c1c4e1b5c30e7d31af7b21d097867c24a
Vrentadax <vrentadax@outlook.com>
2022-04-28 16:37:41 -0600

    restructured some - still error hunting

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
-const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 
