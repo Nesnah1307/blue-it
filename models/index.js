311f437133ff020163f2abcaf8ccca58dbd31fee
Neo <fullstackneo@gmail.com>
2022-05-02 13:44:34 -0600

    exports all models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,21 @@
+const Comment = require('./Comment');
+const Difficulty = require('./Difficulty');
+const Encounter = require('./Encounter');
+const Language = require('./Language');
+const Level = require('./Level');
+const Post = require('./Post');
+const Star = require('./Star');
+const Type = require('./Type');
 const User = require('./User');
 
-
-module.exports = { User };
+module.exports = {
+  Comment,
+  Difficulty,
+  Encounter,
+  Language,
+  Level,
+  Post,
+  Star,
+  Type,
+  User,
+};
