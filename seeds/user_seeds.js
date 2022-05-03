aaa8e29b1d69a343880a830cc76e31baf93960d0
Neo <fullstackneo@gmail.com>
2022-05-02 23:04:32 -0600

    fix user name in seeds file

--- seeds/user_seeds.js
+++ seeds/user_seeds.js
@@ -5,7 +5,7 @@ function generateData() {
   let dataArray = [];
   for (let i = 0; i < 50; i++) {
     dataArray.push({
-      username: faker.name.findName(),
+      username: faker.internet.userName(),
       email: faker.internet.email(),
       password: '1234',
       membership: Math.floor(Math.random() * 2),
