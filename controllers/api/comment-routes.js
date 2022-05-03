3364673ea0cc3af003b273b3dd978e1005bcbb65
MackH <96394025+Nesnah1307@users.noreply.github.com>
2022-05-02 19:08:48 -0600

    finished comment routes and created authorization

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -10,3 +10,42 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+router.post('/', allowed, (req, res) => {
+    if (req.session) {
+        Comments.create({
+            comment_text: req.body.comment_text,
+            user_id: req.session.user_id,
+            post_id: req.body.post_id
+        })
+        .then(dbCommentsData => res.json(dbCommentsData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
+});
+
+router.delete('/:id', allowed, (req, res) => {
+    if (req.session) {
+        Comments.destroy({
+            where: {
+                id:  req.params.id
+            }
+        })
+        .then(dbCommentsData => {
+            if (!dbCommentsData) {
+                res.status(404).json({ message: 'No comment found with this id' });
+                return;
+            }
+            res.json(dbCommentsData);  
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
+});
+
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const commentRoutes = require('./comment-routes.js');
 
 router.use('/users', userRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
